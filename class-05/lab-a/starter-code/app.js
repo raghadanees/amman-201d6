53e7678cdef67115051808ee84dc91bcdd95244e
raghadanees <raghad.anees@hotmail.com>
2020-06-11 14:30:48 +0300

    problem1edit

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -10,8 +10,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 var fSum = a+b;
-
-var strin = 'The sum of ' + a + 'and'+ '' + b + 'is' + fSum ;
+var strin = 'The sum of 4 and 7 is 11.';
+//var strin = 'The sum of ' + a + 'and'+ '' + b + 'is' + fSum ;
 
 return[fSum , strin] 
 }
@@ -22,7 +22,7 @@ sum();
 
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
